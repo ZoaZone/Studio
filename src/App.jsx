7b4aa46f7d586ba129e2ccd26d1a7d6a06582de5
Zoa Zone <vetnpet@gmail.com>
2026-03-26 15:57:56 +0530

    feat: add WidgetHost public route

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 
+import WidgetHost from "@/pages/WidgetHost";
 import Home from './pages/Home';
 import Pricing from './pages/Pricing';
 import Dashboard from './pages/Dashboard';
@@ -66,7 +67,8 @@ const AuthenticatedApp = () => {
 
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/WidgetHost" element={<WidgetHost />} />
+        <Route path="/" element={<Home />} />
       <Route path="/pricing" element={<Pricing />} />
       <Route path="/onboarding" element={<PostPaymentOnboarding />} />
 
